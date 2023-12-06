fe2bbe5bfff725386e2366b54581dd59fad27203
Kevin Garcia <gkevinyamil@gmail.com>
2023-12-05 19:18:48 -0600

    Add geolocation functionality to getUserLocation

--- src/helpers/getUserLocation.ts
+++ src/helpers/getUserLocation.ts
@@ -1,4 +1,31 @@
+import geolocationFunction from "../../netlify/edge-functions/geo"; // Reemplaza con el nombre de tu archivo de función en el borde
+
+
 export const getUserLocation = async (): Promise<[number, number]> => {
+
+	const fetchGeolocation = async () => {
+		new Promise((resolve, reject) => {
+			//get geolocation from netlify edge function
+			fetch('/geo')
+				.then((response) => {
+					console.log(response)
+					return response.json();
+				})
+				.then((data) => {
+					localStorage.setItem("longitude", data.geo.longitude.toString());
+					localStorage.setItem("latitude", data.geo.latitude.toString());
+					resolve([data.geo.longitude, data.geo.latitude]);
+				})
+				.catch((err) => {
+					console.log(err);
+					reject();
+				});
+
+		});	
+		
+	};
+	  fetchGeolocation();
+
 	return new Promise((resolve, reject) => {
 		navigator.geolocation.getCurrentPosition(
 			({ coords }) => {
@@ -12,5 +39,6 @@ export const getUserLocation = async (): Promise<[number, number]> => {
 				reject();
 			}
 		);
+		
 	});
 };
