31652c0b93f3e23c2dd30394e91f7924d8bf2fdb
Kevin Garcia <gkevinyamil@gmail.com>
2023-12-05 20:35:28 -0600

    Update netlify.toml and remove geo.ts

--- netlify/edge-functions/geo.ts
+++
@@ -1,8 +0,0 @@
-import type { Handler } from "@netlify/functions"
-
-export const handler: Handler = async (event, context) => {
-  return {
-    body: JSON.stringify({ geo: context.clientContext }),
-    statusCode: 200,
-  }
-}
