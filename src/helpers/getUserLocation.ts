58cec519869a10b56ba84570e3c9b7290daa1d11
Kevin Garcia <gkevinyamil@gmail.com>
2023-12-05 21:34:02 -0600

    Refactor getUserLocation function to fetch geolocation data from Netlify Function

--- src/helpers/getUserLocation.ts
+++ src/helpers/getUserLocation.ts
@@ -1,40 +1,74 @@
-import edgeFunction  from "../../netlify/edge-functions/geo";
-export const getUserLocation = async (): Promise<[number, number]> => {
+// export const getUserLocation = async (): Promise<[number, number]> => {
+// 	const fetchGeolocation = async () => {		
+// 		const res2 = await fetch("/geo");
+// 		console.log({res2}, 'res2');
+// 		const json2 = await res2.json();
+// 		console.log(json2);
+// 	};
+// 	  fetchGeolocation();
+// 	return new Promise((resolve, reject) => {
+// 		navigator.geolocation.getCurrentPosition(
+// 			({ coords }) => {
+// 				resolve([coords.longitude, coords.latitude]);
+// 				localStorage.setItem("longitude", coords.longitude.toString());
+// 				localStorage.setItem("latitude", coords.latitude.toString());
+// 			},
+// 			(err) => {
+// 				alert("No se pudo obtener la geolocalización");
+// 				console.log(err);
+// 				reject();
+// 			}
+// 		);
+// 	});
+// };
 
-	const fetchGeolocation = async () => {
-		
-		const res2 = await fetch("/geo");
-		console.log({res2}, 'res2');
-		const json2 = await res2.json();
-		console.log(json2);
+// export const getUserLocation = async (): Promise<[number, number]> => {
+// 	try {
+// 	  const res = await fetch("/geo"); // Reemplaza "/geo" con tu ruta de Netlify Function
+// 	  if (!res.ok) {
+// 		throw new Error(`Failed to fetch data: ${res.status}`);
+// 	  }
+// 	  const locationData = await res.json();
+	  
+// 	  const longitude = locationData.longitude;
+// 	  const latitude = locationData.latitude;
+  
+// 	  localStorage.setItem("longitude", longitude.toString());
+// 	  localStorage.setItem("latitude", latitude.toString());
+  
+// 	  return [longitude, latitude];
+// 	} catch (error) {
+// 	  alert("No se pudo obtener la geolocalización");
+// 	  console.error("Error:", error);
+// 	  throw error;
+// 	}
+//   };
 
-		const response = await fetch('/test');
-		console.log({response}, 'response');
-        const data = await response.json();
-		console.log(data);
+export const getUserLocation = (): Promise<[number, number]> => {
+	return new Promise<[number, number]>((resolve, reject) => {
+	  fetch("/geo") // Reemplaza "/geo" con tu ruta de Netlify Function
+		.then((res) => {
+		  if (!res.ok) {
+			throw new Error(`Failed to fetch data: ${res.status}`);
+		  }
+		  return res.json();
+		})
+		.then((locationData) => {
+		  const longitude = locationData.longitude;
+		  const latitude = locationData.latitude;
+  
+		  localStorage.setItem("longitude", longitude.toString());
+		  localStorage.setItem("latitude", latitude.toString());
+		  sessionStorage.setItem("locationData", JSON.stringify(locationData));
 
-		const res = await fetch("/hello");
-		console.log({res}, 'res');
-		const json = await res.json();
-		console.log(json);
-
-
-	};
-	  fetchGeolocation();
-
-	return new Promise((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(
-			({ coords }) => {
-				resolve([coords.longitude, coords.latitude]);
-				localStorage.setItem("longitude", coords.longitude.toString());
-				localStorage.setItem("latitude", coords.latitude.toString());
-			},
-			(err) => {
-				alert("No se pudo obtener la geolocalización");
-				console.log(err);
-				reject();
-			}
-		);
-		
+		  resolve([longitude, latitude]);
+		})
+		.catch((error) => {
+		  alert("No se pudo obtener la geolocalización");
+		  console.error("Error:", error);
+		  reject(error);
+		});
 	});
-};
+  };
+  
+  
