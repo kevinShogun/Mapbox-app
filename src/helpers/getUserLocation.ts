02c4129b21f7544a1572aed7049822e434b9c40b
Kevin Garcia <gkevinyamil@gmail.com>
2023-12-05 21:11:44 -0600

    Add new edge functions

--- src/helpers/getUserLocation.ts
+++ src/helpers/getUserLocation.ts
@@ -3,17 +3,21 @@ export const getUserLocation = async (): Promise<[number, number]> => {
 
 	const fetchGeolocation = async () => {
 		
-		const response = await fetch('http://localhost:8888/geo');
+		const res2 = await fetch("/geo");
+		console.log({res2}, 'res2');
+		const json2 = await res2.json();
+		console.log(json2);
+
+		const response = await fetch('/test');
+		console.log({response}, 'response');
         const data = await response.json();
 		console.log(data);
 
-		const res = await fetch("/.netlify/edge-functions/geo");
+		const res = await fetch("/hello");
+		console.log({res}, 'res');
 		const json = await res.json();
 		console.log(json);
 
-		const res2 = await fetch("/geo");
-		const json2 = await res2.json();
-		console.log(json2);
 
 	};
 	  fetchGeolocation();
